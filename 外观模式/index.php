1c8868dfdbc2b12d6571ec6940860d8f8923cbca
lifue <micanflee@gmail.com>
2016-11-14 23:53:11 +0800

    代理模式 1114

---
+++ 外观模式/index.php
@@ -0,0 +1,55 @@
+<?php 
+
+/**
+* system A
+*/
+class SystemA
+{
+	public function operationA()
+	{
+		echo "operationA <br>";
+	}
+}
+
+class SystemB
+{
+	public function operationB()
+	{
+		echo "operationB <br>";
+	}
+}
+
+class SystemC
+{
+	public function operationC()
+	{
+		echo "operationC <br>";
+	}
+}
+
+/**
+* Fade
+*/
+class Facade
+{
+	protected $systemA;
+	protected $systemB;
+	protected $systemC;
+	
+	function __construct()
+	{
+		$this->systemA = new SystemA();
+		$this->systemB = new SystemB();
+		$this->systemC = new SystemC();
+	}
+
+	public function myOperation()
+	{
+		$this->systemA->operationA();
+		$this->systemB->operationB();
+		$this->systemC->operationC();
+	}
+}
+
+$facade = new Facade();
+$facade->myOperation();
